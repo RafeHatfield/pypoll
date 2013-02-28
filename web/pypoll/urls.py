59985c87aff32ae57ae907a60bec72dbaf25d8b9
RafeHatfield <rafe.hatfield@suite101.com>
2013-02-28 09:19:35 -0800

    finished basic poll tutorial

--- web/pypoll/urls.py
+++ web/pypoll/urls.py
@@ -1,21 +1,29 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+# urlpatterns = patterns('',
+#     url(r'^polls/$', 'polls.views.index'),
+#     url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
+#     url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+#     url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+#     url(r'^admin/', include(admin.site.urls)),
+# )
+
+# urlpatterns = patterns('polls.views',
+#     url(r'^polls/$', 'index'),
+#     url(r'^polls/(?P<poll_id>\d+)/$', 'detail'),
+#     url(r'^polls/(?P<poll_id>\d+)/results/$', 'results'),
+#     url(r'^polls/(?P<poll_id>\d+)/vote/$', 'vote'),
+# )
+
+# urlpatterns += patterns('',
+#   url(r'^admin/', include(admin.site.urls))
+# )
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pypoll.views.home', name='home'),
-    # url(r'^pypoll/', include('pypoll.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^polls/$', 'polls.views.index'),
-    url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
-    url(r'^admin/', include(admin.site.urls)),
+  url(r'^polls/', include('polls.urls')),
+  url(r'^admin/', include(admin.site.urls))
 )
+
